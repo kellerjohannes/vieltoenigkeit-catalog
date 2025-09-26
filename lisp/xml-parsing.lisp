6bcec47d8d1c51948cf05573c12ca8212adc4177
Johannes Keller <johannkell@gmail.com>
2025-09-26 12:47:42 +0200

    working on relative interval conversion

--- lisp/xml-parsing.lisp
+++ lisp/xml-parsing.lisp
@@ -4,11 +4,7 @@
                       (xmls:parse xml-stream)))
 
 
-'((:metadata (:title "Per non mi dir ch'io moia" :composer "Rossi"))
-  (:music ((1 (:i :ottava :➚) (:s :semibrevis) (:i :quarta :➘)))))
-
-'((:metadata (:title "Per non mi dir ch'io moia" :composer "Rossi"))
-  (:music ((1 (:e♯ 5 :semibrevis)))))
+;;; MEI/XML handling
 
 (defun follow-path (mei-data name-strings)
   (if (null name-strings)
@@ -70,9 +66,95 @@
                                                 measures))))
             voice-ns)))
 
+;;; Generating raw score info, (:note [pname] [accid] [oct] [dur]) -> (:note "a" "s" "2" "1")
+
 (defun read-mei (mei-document)
+  "Returns score information in the form of 'score-expression', which is a proper list."
   (if (string= (xmls:node-name mei-document) "mei")
       `((:metadata (:title ,(read-title mei-document)
                     :composer ,(read-composer mei-document)))
         (:music ,(read-music mei-document)))
       (error "No MEI document.")))
+
+
+
+
+;;; Converting score info, (:note "a" "s" "2" "1") -> (:note :a♯ 2 :semibrevis)
+
+(defparameter *accid-dict* '(("s" . "♯")
+                             ("f" . "♭")))
+
+(defun lookup-accid (accid-string)
+  (cdr (assoc accid-string *accid-dict* :test #'string=)))
+
+(defparameter *dur-dict* '(("breve" . :brevis)
+                           ("1" . :semibreivs)
+                           ("2" . :minima)
+                           ("4" . :semiminima)
+                           ("8" . :fusa)
+                           ("16" . :semifusa)))
+
+(defun lookup-dur (dur-string)
+  (cdr (assoc dur-string *dur-dict* :test #'string=)))
+
+(defun translate-note (note-expression)
+  (list :note (alexandria:make-keyword (format nil
+                                               "~a~@[~a~]"
+                                               (string-upcase (second note-expression))
+                                               (lookup-accid (third note-expression))))
+        (parse-integer (fourth note-expression))
+        (lookup-dur (fifth note-expression))))
+
+(defun translate-rest (rest-expression)
+  (list :rest (lookup-dur (second rest-expression))))
+
+(defun translate-note-or-rest (note-or-rest)
+  (case (first note-or-rest)
+    (:note (translate-note note-or-rest))
+    (:rest (translate-rest note-or-rest))))
+
+(defun translate-pitch-info (score-expression)
+  "Returns a score expression (proper list) with converted expressions for notes and rests."
+  (list (first score-expression)
+        (list :music
+              (mapcar (lambda (voice)
+                        (append (list (first voice))
+                                (mapcar (lambda (note-or-rest)
+                                          (translate-note-or-rest note-or-rest))
+                                        (rest voice))))
+                      (second (second score-expression))))))
+
+
+;;; Converting to relative intervals, (:note :a♯ 2 :semibrevis) -> (:i :tono :➚) (:s :semibrevis)
+
+;; e b d c♯ c
+
+(defparameter *relative-intervals-dict*
+  '(((:e :b) ((0 (:quinta :➚ 0))
+              (1 (:duodecii :➚))))))
+
+(defun lookup-relative-interval (origin target)
+  (let ((octave-delta (- (third target) (third origin))))
+    ))
+
+(defun add-to-interval-path (origin-note new-note)
+  (case (first new-note)
+    (:rest (list (list :t (second new-note))))
+    (:note (list (append (list :i)
+                         (lookup-relative-interval origin-note new-note))
+                 (list :s (fourth new-note))))))
+
+(defun converto-to-relative (score-expression origin-note)
+  (let ((origin origin-note))
+    (list (first score-expression)
+          (list :music
+                (mapcar (lambda (voice)
+                          (append (list (first voice))
+                                  (alexandria:mappend
+                                   (lambda (note-or-rest)
+                                     (prog1
+                                         (add-to-interval-path origin note-or-rest)
+                                       (when (eq (first note-or-rest) :note)
+                                         (setf origin note-or-rest))))
+                                                      (rest voice))))
+                        (second (second score-expression)))))))
